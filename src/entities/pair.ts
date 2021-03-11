655c29b75823af5391d3e292ed7a1ee9d456432f
caduceus33 <marcrodriguez93@gmail.com>
2021-03-11 22:20:47 +0100

    lp name

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -55,8 +55,8 @@ export class Pair {
       tokenAmounts[0].token.chainId,
       Pair.getAddress(tokenAmounts[0].token, tokenAmounts[1].token),
       18,
-      'Igni-LP',
-      'IgniLiquidity'
+      'IGNI-LP',
+      'IgniSwap LP'
     )
     this.tokenAmounts = tokenAmounts as [TokenAmount, TokenAmount]
   }
